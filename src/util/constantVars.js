0b5a8ae2e6e317b1e8ef6e4a7bf87a0cf605b8e1
Mark Fuller <Mark_Fuller1@baylor.edu>
2022-05-17 19:25:11 -0700

    change remote ip

--- src/util/constantVars.js
+++ src/util/constantVars.js
@@ -1,11 +1,11 @@
 
 let backend_url = "";
 if (window.location.hostname === "localhost") {
-  backend_url = "http://192.168.1.67:8080";
+  backend_url = "http://192.168.1.49:8080";
   
 } else {
   //we aren't running locally
-  backend_url = "http://192.168.1.67:8080";
+  backend_url = "http://23.241.58.48:8080";
 }
 
 exports.backend_url = backend_url;
